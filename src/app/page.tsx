2ddd3f8d61730b5ccf19bbdc301b547c5bb80eab
Omori0219 <oomoris0219@gmail.com>
2025-01-24 22:36:34 +0900

    feat: ✨ 動的レンダリングを有効化し、常に最新のデータを表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { TimelineSection } from "@/components/post/TimelineSection";
 import { PostFormSection } from "@/components/post/PostFormSection";
 
